87f4d78e37431d92c98cbe14e682772c31e9c366
Ryan <beomagi@gmail.com>
2021-09-04 10:33:33 -0400

    update stuff...

--- demo/demo-server.py
+++ demo/demo-server.py
@@ -9,8 +9,10 @@ import threading
 import os
 import binascii
 import subprocess
+import glob
 
 threadlist={}
+filelist=glob.glob(r'./pages/*') #globally permitted file list
 
 def loadfile(filename):
     """load a file and return data"""
@@ -23,7 +25,11 @@ def loadfile(filename):
 
 def loadpage(pagename):
     """restricts loadfile to pages within a specific folder"""
-    return loadfile("./pages/{}.html".format(pagename))
+    fullname="./pages"+pagename
+    if fullname in filelist:
+        return loadfile(fullname)
+    print("you asked for {}".format(fullname))
+    print(json.dumps(filelist,indent="  "))
 
 def writetofile(astring, filename):
     with open(filename,"w") as fh:
@@ -35,9 +41,9 @@ def randID():
 class Handler(http.server.BaseHTTPRequestHandler):
     '''   use our own handler functions '''
 
-    def sendtextinfo(self, code, text):
+    def sendtextinfo(self, code, text, ctype):
         self.send_response(code)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', ctype)
         self.end_headers()
         if type(text)==type([]):
             for lines in text:
@@ -49,20 +55,22 @@ class Handler(http.server.BaseHTTPRequestHandler):
         '''   handle get   '''
         tnow = time.time()
         gnow = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(tnow)) #Formatted UTC time
+        ctype = "text/html"
 
         parsed_data = urllib.parse.urlparse(self.path)
         location=parsed_data.geturl().lower()
 
         message=""
         if location == "/":
-            message=loadpage("mainpage")
-
-        if location == "/ec2":
-            message=loadpage("ec2")
-            message=message.replace("#IDREP1#",randID())
+            message=loadpage("/mainpage.html")
+        for filestypes in [".css",".htm",".html",".js"]:
+            if location.endswith(filestypes):
+                message=loadpage(location)
+                if filestypes==".css": ctype="text/css"
+                if filestypes==".js": ctype="text/javascript"
 
         print(parsed_data.geturl())
-        self.sendtextinfo(200,message)
+        self.sendtextinfo(200,message,ctype)
 
     def do_POST(self):
         '''   handle post like rest API   '''
--- demo/scripts/aws_preprod_expiry.sh
+++
@@ -1,1 +0,0 @@
-cat ~/.aws/credentials | egrep "^\[|expires" |tr -d "\n"|sed 's_\[_\n\[_g'| grep 'tr-central-preprod' | awk -F '=' '{print $2}'
